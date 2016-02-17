0df9189878e3b573785936a143756bfa0f632d8e
Jakob Erdmann <jakob.erdmann@dlr.de>
2016-02-17 15:07:55 +0000

    {sharps} fix #2155
    git-svn-id: file:///home/behr_mi/git/sumo_synched/branches/sharps@20005 afbd958f-9f77-42d5-a016-97a22340ccf4

--- src/microsim/MSVehicle.cpp
+++ src/microsim/MSVehicle.cpp
@@ -2682,9 +2682,9 @@ void
 MSVehicle::setBlinkerInformation() {
     switchOffSignal(VEH_SIGNAL_BLINKER_RIGHT | VEH_SIGNAL_BLINKER_LEFT);
     int state = getLaneChangeModel().getOwnState();
-    if ((state & LCA_LEFT) != 0) {
+    if ((state & LCA_LEFT) != 0 && (state & LCA_SUBLANE) == 0) {
         switchOnSignal(VEH_SIGNAL_BLINKER_LEFT);
-    } else if ((state & LCA_RIGHT) != 0) {
+    } else if ((state & LCA_RIGHT) != 0 && (state & LCA_SUBLANE) == 0) {
         switchOnSignal(VEH_SIGNAL_BLINKER_RIGHT);
     } else if (getLaneChangeModel().isChangingLanes()) {
         if (getLaneChangeModel().getLaneChangeDirection() == 1) {
