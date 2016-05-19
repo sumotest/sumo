f699458bb8c411dd44a91ba54735b71057bfe0c0
Gregor Laemmel <gregor.laemmel@dlr.de>
2016-05-19 18:43:49 +0000

    {gRPC} bgfx run.sh
    git-svn-id: file:///home/behr_mi/git/sumo_synched/branches/gRPC@20762 afbd958f-9f77-42d5-a016-97a22340ccf4

--- sumo/build/docker/run.sh
+++ sumo/build/docker/run.sh
@@ -4,14 +4,14 @@ SUMO_ROOT=/root/gRPC/sumo/
 SCRIPTS_ROOT=$SUMO_ROOT/build/docker/
 CONF_ROOT=$SUMO_ROOT/tests/complex/hybrid_sumo_jps
 cd
-mkdir /root/mnt/$OUT_DIR
+mkdir $OUT_DIR
 echo "running hybrid simulation"
 if [ -f "/usr/local/bin/jpscore" ]; then
 	jpscore --as-a-service -p 9000 --dump-scenario $OUT_DIR &
 else 
 	echo "jpscore is not installed; going to build an install the latest version"
 	$SCRIPTS_ROOT/update_jps.sh
-	jpscore --as-a-service -p 9000 --dump-scenario /$OUT_DIR &
+	jpscore --as-a-service -p 9000 --dump-scenario $OUT_DIR &
 fi
 if [ -f "/usr/local/bin/sumo" ]; then
 	cd
